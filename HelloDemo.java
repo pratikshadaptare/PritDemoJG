72b293a3ebfff6f59b456c148e601916c938c93d
Daptare <pratikshadaptare19@gmail.com>
2019-02-11 21:00:56 +0530

    Adding project content

---
+++ HelloDemo.java
@@ -0,0 +1,12 @@
+public class HelloDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0;i<=10;i++) {
+			System.out.println("Its a demo program : "+i);
+		}
+
+	}
+
+}
